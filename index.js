137587c3694306c9170a93af2afae05366357f65
cagataycali <calicagatay@icloud.com>
2016-08-24 18:20:16 -0700

    Notifier added

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var async = require('async');
 var E = require('3x3c');
+var updateNotifier = require('update-notifier');
+var pkg = require('./package.json');
+updateNotifier({pkg}).notify();
 
 module.exports = function () {
   return new Promise(function (resolve, reject) {
     E(`git symbolic-ref HEAD`)
-      .then((value) => {resolve(value.split('/').pop(-1))})
+      .then((value) => {resolve(value.split('/').pop(-1).trim())})
       .catch((err) => {reject(err)});
   });
 }
